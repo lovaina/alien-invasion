0ddc0f20c7b27cd6b05f5665b3ebdf8cfb19c869
mireia <mireia@actualweb.es>
2017-06-05 14:40:03 +0200

    Ajustando la velocidad de la nave

--- ship.py
+++ ship.py
@@ -2,8 +2,9 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         self.screen = screen
+        self.ai_settings = ai_settings
 
         #Load the ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
@@ -14,15 +15,17 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        #Store a decimal value for the ship's center
+        self.center = float(self.rect.centerx)
         #Movement flag
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         if self.moving_right:
-            self.rect.centerx += 1
+            self.rect.centerx += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.rect.centerx -= self.ai_settings.ship_speed_factor
 
     def blitme(self):
         """Draw the ship at its current location"""
