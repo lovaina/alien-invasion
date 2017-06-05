e946cda347b716a5f1c9c46726949f8764de3841
mireia <mireia@actualweb.es>
2017-06-05 13:53:36 +0200

    Añadido background azul

--- settings.py
+++ settings.py
@@ -1,8 +1,10 @@
 class Settings():
+    BG_BLUE = (230, 247, 255)
+    BG_GREY = (230,230,230)
 
     def __init__(self):
         """Initialize all settings for ALien INvasion"""
         #Screen
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230,230,230)
+        self.bg_color = self.BG_BLUE
