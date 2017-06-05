c3ef11eaeb6d63fcce30b183831bfc390340455b
mireia <mireia@actualweb.es>
2017-06-05 13:43:09 +0200

    Añadida nave

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
 
@@ -13,6 +14,7 @@ def run_game():
         ai_settings.screen_width, ai_settings.screen_height
     ))
     pygame.display.set_caption("Alien Invasion")
+    ship = Ship(screen)
 
     # Start the main loop for the game
     while True:
@@ -24,6 +26,7 @@ def run_game():
 
         # Redraw the screen during each pass through the loop
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         #Make the most recently drawn screen visible
         pygame.display.flip()
