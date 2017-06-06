90058404f633b1a8e4639f7fe447035a16138db7
mireia <mireia@actualweb.es>
2017-06-06 13:23:26 +0200

    Añadiendo movimiento a los aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,6 +27,7 @@ def run_game():
         ship.update()
         bullets.update()
         gf.update_bullets(bullets)
+        gf.update_aliens(ai_settings, aliens)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
