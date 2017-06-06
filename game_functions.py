6487d338323ab161ad3bcfdc7c7b95a1e0813106
mireia <mireia@actualweb.es>
2017-06-06 12:26:24 +0200

    Refactorizada la creación de la flota

--- game_functions.py
+++ game_functions.py
@@ -60,13 +60,22 @@ def create_fleet(ai_settings, screen, aliens):
     # Create an alien and find the number of aliens in a row.
     # Spacing between each alien is equal to one alien width.
     alien = Alien(ai_settings, screen)
-    alien_width = alien.rect.width
-    available_space_x = ai_settings.screen_width - 2 * alien_width
-    number_aliens_x = int(available_space_x / (2 * alien_width))
+    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
 
     # Create first row
     for alien_number in range(number_aliens_x):
-        alien = Alien(ai_settings, screen)
-        alien.x = alien_width + 2 * alien_width * alien_number
-        alien.rect.x = alien.x
-        aliens.add(alien)
+        create_alien(ai_settings, alien_number, aliens, screen)
+
+
+def create_alien(ai_settings, alien_number,  aliens, screen):
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    aliens.add(alien)
+
+
+def get_number_aliens_x(ai_settings, alien_width):
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    return number_aliens_x
