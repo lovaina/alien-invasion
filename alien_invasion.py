ca677da9b8152f1216c6ae69be71ddd29ef054a2
mireia <mireia@actualweb.es>
2017-06-06 12:56:01 +0200

    Añadida flota completa

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,7 +19,7 @@ def run_game():
     ship = Ship(ai_settings,screen)
     bullets = Group()
     aliens = Group()
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # Start the main loop for the game
     while True:
--- game_functions.py
+++ game_functions.py
@@ -56,22 +56,26 @@ def fire_bullets(ai_settings, screen, ship, bullets):
         bullets.add(new_bullet)
 
 
-def create_fleet(ai_settings, screen, aliens):
+def create_fleet(ai_settings, screen, ship, aliens):
     # Create an alien and find the number of aliens in a row.
     # Spacing between each alien is equal to one alien width.
     alien = Alien(ai_settings, screen)
     number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+    number_rows = get_number_rows(ai_settings, ship.rect.height, alien.rect.height)
 
     # Create first row
-    for alien_number in range(number_aliens_x):
-        create_alien(ai_settings, alien_number, aliens, screen)
+    for row_number in range(number_rows):
+        for alien_number in range(number_aliens_x):
+            create_alien(ai_settings, screen, aliens, alien_number,
+                         row_number)
 
 
-def create_alien(ai_settings, alien_number,  aliens, screen):
+def create_alien(ai_settings, screen, aliens, alien_number, number_rows):
     alien = Alien(ai_settings, screen)
     alien_width = alien.rect.width
     alien.x = alien_width + 2 * alien_width * alien_number
     alien.rect.x = alien.x
+    alien.rect.y = alien.rect.height + 2 * alien.rect.height * number_rows
     aliens.add(alien)
 
 
@@ -79,3 +83,9 @@ def get_number_aliens_x(ai_settings, alien_width):
     available_space_x = ai_settings.screen_width - 2 * alien_width
     number_aliens_x = int(available_space_x / (2 * alien_width))
     return number_aliens_x
+
+
+def get_number_rows(ai_settings, ship_height, alien_height):
+    available_space_y = (ai_settings.screen_height - (3 * alien_height) - ship_height)
+    number_rows = int(available_space_y / (2 * alien_height))
+    return number_rows
