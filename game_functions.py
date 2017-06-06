c3a0ddb684907c2fc9e5a245a8d07fa1637f7181
mireia <mireia@actualweb.es>
2017-06-06 09:12:53 +0200

    Refactorizar: extraidas funciones para las balas

--- game_functions.py
+++ game_functions.py
@@ -20,9 +20,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     if event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullets(ai_settings, screen, ship, bullets)
 
 def check_keyup_events(event, ship):
     """Respond to key releases"""
@@ -41,3 +39,14 @@ def update_screen(ai_settings, screen, ship, bullets):
 
     # Make the most recently drawn screen visible
     pygame.display.flip()
+
+def update_bullets(bullets):
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+
+def fire_bullets(ai_settings, screen, ship, bullets):
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
